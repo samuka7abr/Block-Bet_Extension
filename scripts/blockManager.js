dec81d4753b3cce26c6c1423217d4f9c30c17020
Samuel Abrão <163413922+samuka7abr@users.noreply.github.com>
2024-10-31 09:06:47 -0300

    update

--- scripts/blockManager.js
+++
@@ -1,7 +0,0 @@
-export const addSiteToBlock = (site) => {
-    chrome.storage.local.get({ userBlockedSites: [] }, data => {
-        const userBlockedSites = data.userBlockedSites;
-        userBlockedSites.push(site);
-        chrome.storage.local.set({ userBlockedSites });
-    });
-};
--- scripts/storageManager.js
+++
@@ -1,9 +0,0 @@
-export const getUserBlockedSites = (callback) => {
-    chrome.storage.local.get({ userBlockedSites: [] }, data => {
-        callback(data.userBlockedSites);
-    });
-};
-
-export const setUserBlockedSites = (sites) => {
-    chrome.storage.local.set({ userBlockedSites: sites });
-};
